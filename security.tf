0efa3a5e5fd46104b87927c32a383d5a672053ca
Preksha <malupreksha98@gmsil.com>
2024-06-14 22:48:33 +0530

    security group

--- security.tf
+++ security.tf
@@ -10,7 +10,7 @@ resource "aws_security_group" "batch-security-group" {
 
 resource "aws_vpc_security_group_ingress_rule" "batch-security-group-ingress" {
   security_group_id = aws_security_group.batch-security-group.id
-  cidr_ipv4         = aws_vpc.main.cidr_block
+  cidr_ipv4         = "0.0.0.0/0"
   from_port         = 443
   ip_protocol       = "tcp"
   to_port           = 443
